465497c6df4f207ba3581e0d1e2759230ed3daa4
Danielle Aragao <danielle.s.arag@gmail.com>
2024-05-30 20:53:15 -0300

    Desafio Concluído

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -1,3 +1,4 @@
+
 namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesLista
@@ -29,5 +30,11 @@ namespace TestesUnitarios.Desafio.Console.Services
         {
             return lista.Min();
         }
+
+        public bool MultiplicarNumerosLista(List<int> lista, List<int> resultadoEsperado)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
